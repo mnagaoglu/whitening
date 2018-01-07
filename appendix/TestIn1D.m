868a87f8ae99c48247b322e548405cb01d38d757
mnagaoglu <mnagaoglu@gmail.com>
2018-01-07 13:40:23 -0800

    simulating x-y motion

--- appendix/TestIn1D.m
+++ appendix/TestIn1D.m
@@ -1,6 +1,6 @@
 function TestIn1D
 
-close all;
+close all force;
 clc;
 
 % iterations
@@ -17,18 +17,22 @@ lineLengthPx = 512;
 % define temporal parameters
 T = 1; % seconds 
 Fs = 512; % Hz
-D = [100]; % arcmin^2/sec diffusion constant;
+D = [40 100]; % arcmin^2/sec diffusion constant;
 t = linspace(-T/2,T/2, T*Fs);
 
 % get the list of images 
 fList = dir([pwd filesep 'images' filesep '*.jpg']);
 
+
+wb = waitbar(0,'Please wait...');
+
 for di = 1:length(D)
 
-    W = zeros(N,T*Fs);
+    Wx = zeros(N,T*Fs);
     
     avgPSD = zeros(length(T*Fs), lineLengthPx);
-    noMotionAvgPSD = zeros(lineLengthPx,1);
+    noMotionAvgPSD = avgPSD;
+    
     
     for iter = 1:N
         
@@ -47,31 +51,34 @@ for di = 1:length(D)
         apertureSizeDeg = pixelSizeInDeg * lineLengthPx;
 
         % get a simulated eye movement trace in deg
-        W(iter,:) = SimulateEyeMovements(T, Fs,D(di));
+        Wx(iter,:) = SimulateEyeMovements(T, Fs,D(di));
+        
+        Wy(iter,:) = SimulateEyeMovements(T, Fs,D(di));
 
         % convert that to pixel units
-        eyeMovementsPx = round(W(iter,:) / pixelSizeInDeg);        
+        eyeMovementsPxX = round(Wx(iter,:) / pixelSizeInDeg);     
+        eyeMovementsPxY = round(Wy(iter,:) / pixelSizeInDeg);
 
-        [x,y] = GetInitialPosition(height,width,eyeMovementsPx,lineLengthPx);
+        [x,y] = GetInitialPosition(height,width,eyeMovementsPxX,eyeMovementsPxY,lineLengthPx);
 
         % create the space-time image
         st = zeros(length(T*Fs), lineLengthPx);
         for i=1:length(t)
-            st(i,:) = im(y, x+eyeMovementsPx(i) : x+eyeMovementsPx(i)+lineLengthPx-1);
+            st(i,:) = im(y+eyeMovementsPxY(i), ...
+                         x+eyeMovementsPxX(i) : x+eyeMovementsPxX(i)+lineLengthPx-1);
         end
-        
-%         imwrite(st,'forest.jpg');
-        
-        % remove DC
-        st = st - mean(st(:));
 
         % compute the frequency response
-        [Fst,tf,sf] = GetFFT(t,lineLengthPx, st, pixelSizeInDeg);
-        noFst = fftshift(abs(fft(im(y, x : x+lineLengthPx-1))).^2)';
+        [Fst,tf,sf] = GetFFT(t,lineLengthPx, st - mean(st(:)), pixelSizeInDeg);
+        noFst = abs(fftshift(fft(st - repmat(mean(st,2),1,size(st,2)) ,[],2)));
 
         % sum up PSDs 
-        avgPSD = avgPSD + abs(fftshift(Fst)).^2/(T*Fs*lineLengthPx);
-        noMotionAvgPSD = noMotionAvgPSD + noFst/(T*Fs);
+        avgPSD = avgPSD + (2*Fst).^2/(T*Fs*lineLengthPx);
+        noMotionAvgPSD = noMotionAvgPSD + (2*noFst).^2/(T*Fs);
+        
+        
+        waitbar(iter/N,wb,sprintf('D: %d, progress %% %0.f', D(di), 100*iter/N))
+        
     end
 
     % normalize to make it average rather than sum
@@ -79,29 +86,28 @@ for di = 1:length(D)
     noMotionAvgPSD = noMotionAvgPSD / N;
 
     figure('name',sprintf('D:%d',D(di)),'units',...
-        'normalized','outerposition',[0.1891    0.0908    0.68    0.7375]); 
+        'normalized','outerposition',[0.1891    0.0908   0.7417    0.8905]); 
     subplot(2,3,1)
-    plot(t,W','-r','LineWidth',2);
+    plot(t,Wx','-r','LineWidth',2);
     hold on;
-    plot(t,W(end,:)','-k','LineWidth',2);
-    plot([0 0],[-.5 .5]*apertureSizeDeg + W(N,find(t>0,1))*[1 1],'-k','LineWidth',2);
+    plot(t,Wx(end,:)','-k','LineWidth',2);
+    plot([0 0],[-.5 .5]*apertureSizeDeg + Wx(N,find(t>0,1))*[1 1],'-k','LineWidth',2);
     set(gca,'fontsize',16);
     xlabel('time (sec)');
     ylabel('position (deg)');
-    ylim([min(W(N,:))-apertureSizeDeg/2 max(W(N,:))+apertureSizeDeg/2]);
+    ylim([min(Wx(N,:))-apertureSizeDeg/2 max(Wx(N,:))+apertureSizeDeg/2]);
     axis square;
     
     subplot(2,3,2)
-    imagesc([min(W(N,:))-apertureSizeDeg/2 max(W(N,:))+apertureSizeDeg/2],[min(t) max(t)],st);
+    imagesc([min(Wx(N,:))-apertureSizeDeg/2 max(Wx(N,:))+apertureSizeDeg/2],[min(t) max(t)],st);
     colormap(gca,gray);
     ylabel('time (sec)');
     xlabel('position (deg)');
     set(gca,'fontsize',16);
-    title('eye motion','color','r')
     axis square;
     
     subplot(2,3,3)
-    imagesc([min(sf) max(sf)],[min(tf) max(tf)],(20*log10(avgPSD)));
+    imagesc([min(sf) max(sf)],[min(tf) max(tf)],(10*log10(avgPSD)));
     colormap(gca,gray);
     ylabel('temporal freq. (Hz)');
     xlabel('spatial freq. (cpd)');
@@ -110,9 +116,9 @@ for di = 1:length(D)
     
 
 %     subplot(2,3,5)
-%     plot(sf, 20*log10(sum(2*avgPSD, 1)),'-r','LineWidth',2);
+%     plot(sf, 10*log10(sum(2*avgPSD, 1)),'-r','LineWidth',2);
 %     hold on;
-%     plot(sf, 20*log10(sum(2*noMotionAvgPSD  + 0.00001, 1)),'-b','LineWidth',2);
+%     plot(sf, 10*log10(sum(2*noMotionAvgPSD  + 0.00001, 1)),'-b','LineWidth',2);
 %     ylabel('power (dB)');
 %     xlabel('spatial freq. (cpd)');
 %     set(gca,'fontsize',16,'yscale','linear','xscale','log');
@@ -120,20 +126,26 @@ for di = 1:length(D)
 %     grid on;
     
     subplot(2,3,5)
-    plot(sf, 20*log10(2*avgPSD(find(tf>10,1),:)),'-r','LineWidth',2);
+    cols = jet(T*Fs/2-1);
+%     for lx = 1 : T*Fs/2-1
+%         plot(sf, 10*log10(avgPSD(T*Fs/2 + lx,:)),'-','Color',cols(lx,:),'LineWidth',2);
+%         hold on;
+%         plot(sf, 10*log10(noMotionAvgPSD(T*Fs/2 + lx,:)),'--','Color',cols(lx,:),'LineWidth',2);
+%     end
+    plot(sf, mean(10*log10(avgPSD),1),'-r','LineWidth',2);
     hold on;
-    plot(sf, 20*log10(2*noMotionAvgPSD),'-b','LineWidth',2);
+    plot(sf, mean(10*log10(noMotionAvgPSD),1),'-b','LineWidth',2);
     ylabel('power (dB)');
     xlabel('spatial freq. (cpd)');
     set(gca,'fontsize',16,'yscale','linear','xscale','log');
-    xlim([.3 60])
+    xlim([.5 60])
     grid on;
     axis square;
     
     subplot(2,3,6)
-    plot(tf, 20*log10(sum(2*avgPSD, 2)),'-r','LineWidth',2);
-    hold on;
-    plot(tf, 20*log10(0.00001),'-b','LineWidth',2);
+    plot(tf, mean(10*log10(avgPSD), 2),'-r','LineWidth',2);
+%     hold on;
+%     plot(tf, 10*log10(0.00001),'-b','LineWidth',2);
     ylabel('power (dB)');
     xlabel('temporal freq. (Hz)');
     set(gca,'fontsize',16,'yscale','linear','xscale','log');
@@ -142,20 +154,29 @@ for di = 1:length(D)
     axis square;
 end
 
+delete(wb);
 
-function [x,y] = GetInitialPosition(height,width,eyeMovementsPx,lineLengthPx)
+
+function [x,y] = GetInitialPosition(height,width,eyeMovementsPxX,eyeMovementsPxY,lineLengthPx)
 
 % get a random starting point on the image, avoiding the boundaries by
-% taking into account the max and min eye position. we do not need to check
-% boundaries for y, since we will be simulating only horizontal eye
-% movements
-y = randi(height,1); 
+% taking into account the max and min eye position. 
 while true
     x = randi(width,1);
-    if (x + min(eyeMovementsPx)) > 1 && (x + max(eyeMovementsPx) + lineLengthPx) < width
+    if (x + min(eyeMovementsPxX)) > 1 && (x + max(eyeMovementsPxX) + lineLengthPx) < width
         break;
     end
 end
+
+
+while true
+    y = randi(height,1); 
+    if (y + min(eyeMovementsPxY)) > 1 && (y + max(eyeMovementsPxY)) < height
+        break;
+    end
+end
+
+
 
 
 function [Fst,tf,sf] = GetFFT(t,lineLengthPx, st, pixelSizeInDeg)
@@ -168,7 +189,7 @@ tf = (1/dt) * linspace(-.5, .5, Lt);
 sf = (1/pixelSizeInDeg) * linspace(-.5, .5, Ls);
 
 % take the FFt
-Fst = fft2(st);
+Fst = abs(fftshift(fft2(st)));
 
 
 
