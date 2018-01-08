ac682e22a4921f8646bbf09a1bd1735cee82ee0c
mnagaoglu <mnagaoglu@gmail.com>
2018-01-08 10:26:59 -0800

    final touches to generate publication figures

--- appendix/TestStitching.m
+++ appendix/TestStitching.m
@@ -9,48 +9,62 @@ function TestStitching
 close all;
 clc;
 
-global organizedData;
-filePath = '../../ganglion/organizedData.mat';
-load(filePath,'organizedData');
+isRealEM = 1;
 
 
-isPlotTime = 0;
+if isRealEM
+    global organizedData;
+    filePath = '../../ganglion/organizedData.mat';
+    load(filePath,'organizedData');
+end
+
+isPlotTime = 1;
+simulateFs = 1;
 
 % iterations
-N = 5000;
+N = 100;
 
 % define temporal parameters
-T = fliplr([1/8 1/4 1/2 1 2 4]); % seconds 
-Fs = 512; % Hz
+if ~simulateFs
+    T = fliplr([1/16 1/8 1/4 1/2 1 2 4]); % seconds 
+    Fs = repmat(1024,1,length(T)); % Hz
+else
+    Fs = fliplr([128 256 512 1024 2048]); % Hz
+    T = ones(1,length(Fs)); % seconds 
+end
+
 D = 40; % arcmin^2/sec diffusion constant;
 
-figure('units','normalized','outerposition',[-0.6750   -0.2000    0.6750    0.5000]);
+figure('units','normalized','outerposition',[-0.6750   -0.2000    0.5554    0.3895]);
 for i=1:length(T)
 
-    FW = zeros(1,round(T(i)*Fs));
+    FW = zeros(1,round(T(i)*Fs(i)));
 
     try
         for iter=1:N
             
-%             % simulate eye motion
-%             [W, t, dt] = SimulateEyeMovements(T(i),Fs,D);
+            if ~isRealEM
+                % simulate eye motion
+                [W, t, dt] = SimulateEyeMovements(T(i),Fs(i),D);
+            else
+                % get real eye motion
+                [W, t, dt] = GetRealEyeMovements(T(i),Fs(i));
+            end
             
-            % get real eye motion
-            [W, t, dt] = GetRealEyeMovements(T(i),Fs);
-
             % take fft
-            tempFW = abs(fftshift(fft(W)));
-            FW = FW + 2* (tempFW.^2 / (T(i)*Fs));
+            tempFW = 2*abs(fftshift(fft(W)));
+            FW = FW + (tempFW.^2 / (T(i)*Fs(i)));
 
             if isPlotTime
                 subplot(1,2,1)
-                plot(t, W,'-','LineWidth',2,'Color',[1 0 0]*i/length(T));
+                plot(t, W,'-','LineWidth',1,'Color',[1 0 0]*i/length(T));
                 hold on;
                 ylabel('position (deg)');
                 xlabel('time (sec)');
-                set(gca,'fontsize',16,'yscale','linear','xscale','linear');
+                set(gca,'fontsize',20,'yscale','linear','xscale','linear');
                 grid on;
                 axis square;
+                box off;
 
             end
         end
@@ -59,7 +73,7 @@ for i=1:length(T)
     end
 
     % take average
-    tf = (1/dt) * linspace(-.5, .5, T(i)*Fs);
+    tf = (1/dt) * linspace(-.5, .5, T(i)*Fs(i));
     FW = FW / N;
 
     
@@ -74,8 +88,25 @@ for i=1:length(T)
     grid on;
     axis square;
     
-    
+
 end
+
+% put a legend
+leg = [];
+for i=1:length(T)
+    if simulateFs
+        leg{i} = mat2str(Fs(i));
+        if i==1
+            title('Sampling rate');
+        end
+    else
+        if i==1
+            title('Time window');
+        end
+        leg{i} = mat2str(T(i));
+    end
+end
+legend(leg);
 
 
 
@@ -96,6 +127,8 @@ dt = diff(t(1:2));
 dW = sqrt(2*diffusion*dt) * randn(1,length(t)-1); 
 W = [zeros(1,1) cumsum(dW,2)];
 
+W = W - W(1);
+
 
 
 
@@ -111,8 +144,8 @@ youngIndices = find(groups == 2);
 subjectIndex = randi(length(youngIndices),1);
 
 % get the time and position traces
-time = organizedData(youngIndices(subjectIndex)).newTime;
-pos = organizedData(youngIndices(subjectIndex)).newPos;
+time = organizedData(youngIndices(subjectIndex)).stitchedTime;
+pos = organizedData(youngIndices(subjectIndex)).stitchedPosition;
 
 % how many samples requested
 nSamples = T*Fs;
